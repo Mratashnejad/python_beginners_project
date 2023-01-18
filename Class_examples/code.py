6978bc7fb784d72800a5818c156f8fedc23581e3
Alireza Atashnejad <32798129+Mratashnejad@users.noreply.github.com>
2023-01-18 17:07:12 +0400

    date is not correct.

--- Class_examples/code.py
+++ Class_examples/code.py
@@ -6,27 +6,37 @@ class parrot():
     name = ""
     lastname = ""
     age = 0
+    year = 0
+    month = 0
+    day = 0
 
 #create object1 from parrot
 parrot1 = parrot()
 parrot1.name = input("Please enter your name : ")
 parrot1.lastname = input("Please Enter your last name : ")
-parrot1.age = int(input("Please enter your Age : "))
+parrot1.year = int(input("Please enter your year : "))
+parrot1.month = int(input("Please enter your month : "))
+parrot1.day =int(input("Please enter your day : "))
 
 calcuteage = 0
 
-def agecalcuter(s):
+def agecalcuter(y,m,d):
     year = datetime.date.today().year
-    print ("we are in : " ,year)
-    if s != 0:
-        calcuteage = year - s
-        return (calcuteage)
+    month = datetime.date.today().month
+    day = datetime.date.today().day
+    print ("we are in : " ,year,month,day)
+    if y != 0 and m !=0 and d !=0:
+        calcuteyear = year - y
+        calcutemonth = m - month
+        calcuteday = day - d
+        return (calcuteyear , calcutemonth ,calcuteday)
     
 
-
 if __name__ == '__main__':
-    age = parrot1.age
-    total = agecalcuter(age)
+    y = parrot1.year
+    m = parrot1.month
+    d = parrot1.day 
+    total = agecalcuter(y,m,d)
 #access attributes : 
     print(f"Dear {parrot1.name} {parrot1.lastname} , you born in  {total}")
 
