259379286c3e7039f6e193a5cfb8f15ce0f5f093
Alireza Atashnejad <32798129+Mratashnejad@users.noreply.github.com>
2023-01-19 17:05:23 +0400

    Done!

---
+++ readfileword/code.py
@@ -0,0 +1,10 @@
+#read text file from current directory and split word inside.
+
+
+with open('readfileword/test.txt','r') as file:
+    for line in file:
+        for word in line.split():
+            print(word)
+
+
+
