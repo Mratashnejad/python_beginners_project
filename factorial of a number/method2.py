c2fbf95026db4356a82e2893fd4ce6ed77bdb5bf
Alireza Atashnejad <32798129+Mratashnejad@users.noreply.github.com>
2023-01-19 23:23:45 +0400

    Done!

---
+++ factorial of a number/method2.py
@@ -0,0 +1,13 @@
+#Python Program for factorial of a number with math
+#
+#
+#
+#Alireza Atashnejad - Python
+
+import math
+
+def factorial (number):
+    return math.factorial(number)
+
+number = int(input("please enter number :"))
+print("Factorial is :",factorial(number))
