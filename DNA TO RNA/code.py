c11a0987ef514dca9e97fc850946e95dcf020850
Alireza Atashnejad <32798129+Mratashnejad@users.noreply.github.com>
2023-02-08 23:39:19 +0400

    update

--- DNA TO RNA/code.py
+++ DNA TO RNA/code.py
@@ -1,13 +1,14 @@
 import stdio 
 
 
-
+#translate dana 
 def translate(dna):
     dna = dna.upper()
     rna = dna.replace("T" ,"U")
     return rna
 
 
+#main 
 if __name__ == '__main__':
     dna = input("please inter your DNA string :")
     rna = translate(dna)
